61b466691a7c2496cdbe90404db048ea1953faeb
rohit0985 <rohitsharma4681@gmail.com>
2023-01-24 11:02:51 +0530

    added

--- db.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const mongoose = require("mongoose")
-
-mongoose.set("strictQuery", false);
-const connection = mongoose.connect(process.env.MONGO_URL)
-
-module.exports = {connection}
